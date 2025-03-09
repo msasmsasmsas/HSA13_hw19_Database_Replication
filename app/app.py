d9ba00549cda49709ac9c5a36e5e0465c44f78ce
HSA13_hw4_Stress_Testing_Approaches_and_tools <msas@ukr.net>
2025-03-09 20:58:37 +0200

    1

---
+++ app/app.py
@@ -0,0 +1,40 @@
+import mysql.connector
+import time
+from datetime import datetime
+
+# Подключение к базе данных
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="rootpass",
+    database="test_db",
+    port=3306
+)
+cursor = conn.cursor()
+
+try:
+    while True:
+        # Подготовка данных для вставки 10 записей
+        data_to_insert = [
+            ("test_name_" + str(i), datetime.now()) for i in range(10)
+        ]
+
+        # Выполнение множественной вставки
+        cursor.executemany(
+            "INSERT INTO test_table (name, timestamp) VALUES (%s, %s)",
+            data_to_insert
+        )
+
+        # Подтверждение транзакции
+        conn.commit()
+        print("10 records inserted")
+
+        # Пауза 5 секунд перед следующей вставкой
+        time.sleep(5)
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
+finally:
+    # Закрытие соединения
+    conn.close()
